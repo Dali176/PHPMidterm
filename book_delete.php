9df62a8e9592c8740d942e00b175af7c3362eeec
dali lewis <dali lewis>
2017-02-22 15:37:14 -0500

    Finished book_delete now need to test

--- book_delete.php
+++ book_delete.php
@@ -4,8 +4,15 @@ include_once('database.php');
 /*//////////////////////*/
 /* YOUR CODE GOES HERE */
 /*/////////////////////*/
+$bookID = $_GET["bookID"];
 
-
+if($bookID != false) {
+    $query = "DELETE FROM books WHERE Id = :book_id";
+    $statement = $db->prepare($query);
+    $statement->bindValue("book_id", $bookID);
+    $success = $statement->execute();
+    $statement->closeCursor();
+}
 // redirect to index page
 header('Location: index.php');
 
